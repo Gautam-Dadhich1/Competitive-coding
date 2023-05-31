ad0b5d63e83cbf4a58c5e99834e40d7d9e9fa685
sharma-gautam1234 <gautamdadhich001@gmail.com>
2023-05-31 10:12:17 +0530

    string hash function added

--- string_hashing.cpp
+++ string_hashing.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
-#define ll long long 
+#include <cmath>
+#define ll long long
 #define nline "\n"
 using namespace std;
 
-// Hashing function 
-const int MOD = 1e9+7;
+// Hashing function
+const int MOD = 1e9 + 7;
 
 ll get_hash(string s)
 {
     ll h = 0;
-    for(char c : s)
-    {
+    for (char c : s)
         h = (h * 31 + (c - 'a' + 1)) % MOD;
-    }
+
     return h;
+
+    // this is the actual implementation, but to reduce the computation we use the above one
+
+    // ll h = 0, i = s.length()-1;
+    // for(char c : s)
+    // {
+    //     h += ((ll)pow(31, i)*(c - 'a' + 1)) % MOD;
+    //     i--;
+    // }
+    // return h;
 }
 
 int main()
 {
     string s = "ab";
-    cout << get_hash(s)<<nline;
+    cout << get_hash(s) << nline;
     return 0;
 }
